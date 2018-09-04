11ffc56d3ba063f0dd6f66ce6f788f8b49bb1d31
Nenad Panic <nenad@mitipi.com>
2018-09-04 10:25:42 +0200

    configure plugin

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # serverless-ngrok-tunnel
-Serverless plugin that creates ngrok public tunnel on localhost.
+Serverless plugin that creates ngrok public tunnel on localhost.  
+Optionally, writes tunnels url to .env file and deletes them after session is over. Useful for when you want to expose url for other applications to use (for example mobile application).
+
+## Installation
+
+Add serverless-ngrok-tunnel to your project:  
+`npm install --save-dev serverless-ngrok-tunnel`  
+
+Then inside your `serverless.yml` file add following entry to the plugins section:  
+```yaml
+plugins:
+  - serverless-ngrok-tunnel
+```
+
+## Usage
+
+First you will need to configure tunnels. In your `serverless.yml` file add:
+```yaml
+custom:
+  ngrokTunnel:
+    envPath: '../.env' # optional. Path to your .env file, relative to serverless.yml file
+    tunnels: # required
+
+      # if you are using `serverless-offline` plugin, expose api gateway
+      - port: 8000 # required
+        envProp: 'API_GATEWAY' # optional. property in .env file to assign url value to
+
+      # or if you are using `serverless-iot-offline` plugin, expose iot endpoint
+      - port: 1884
+        envProp: 'IOT_ENDPOINT'
+        ws: true # optional. expose web-socket url
+        path: '/mqqt' # optional. additional pat to url
+        
+      - port: 9000
+        ngrokOptions: # optional. custom ngrok options
+          authtoken: '12345'
+          region: 'us',
+          subdomain: 'my-subdomain'
+          
+```
+For a list of available ngrok options checkout ngrok [documentation](https://github.com/bubenshchykov/ngrok#options).  
+
+To start tunnel/s run `sls tunnel`.  
+If you are using `serverless-offline` plugin, start offline with option flag: `sls offline start --tunnel=true`.
+
+## Contributing
+
+Yes, please. Checkout [contributing guidelines](./CONTRIBUTING.md).
+
+##Licence
+
+MIT
---
+++ index.js
@@ -0,0 +1,125 @@
+const ngrok = require('ngrok')
+const envFile = require('envfile')
+const path = require('path')
+const fs = require('fs')
+const _ = require('lodash')
+
+/**
+ * Creates public tunnels for provided ports on localhost. Also, writes tunnels url to .env file and deletes them after session is over.
+ */
+class ServerlessTunnel {
+  constructor (serverless, options) {
+    this.serverless = serverless
+    this.log = serverless.cli.log.bind(serverless.cli)
+    this.slsOptions = options
+    this.reconnectTried = false
+    this.noEnvFile = true
+
+    this.commands = {
+      tunnel: {
+        lifecycleEvents: ['init']
+      }
+    }
+
+    // Run tunnels after serverless-offline
+    this.hooks = {
+      'tunnel:init': this.runServer.bind(this, true),
+      'before:offline:start:init': this.runServer.bind(this)
+    }
+  }
+
+  runTunnel ({port, envProp, ws, path, ngrokOptions}) {
+    try {
+      ngrok.connect({
+        addr: port,
+        proto: 'http',
+        region: 'eu',
+        ...(ngrokOptions || {})
+      }, (err, url) => {
+        if (err) {
+          this.log(`Unable to create tunnel: ${err.message}`)
+        } else {
+          this.onConnect(url, envProp, ws, path)
+        }
+      })
+
+      ngrok.on('disconnect', () => this.onTunnelClose())
+      ngrok.on('error', (e) => this.errorHandler(e))
+    } catch (e) {
+      this.errorHandler(e)
+    }
+  }
+
+  onConnect (url, envProp, ws, path) {
+    const tunnel = ws ? url.replace('http', 'ws') : url
+    if (envProp) {
+      this.envContent[envProp] = `${tunnel}${path || ''}`
+      this.log(`${envProp} available at: ${this.envContent[envProp]}`)
+    } else {
+      this.log(`Tunnel created at ${tunnel}${path || ''}`)
+    }
+    this.writeToEnv()
+  }
+
+  errorHandler (e) {
+    this.log(`Tunnels error: ${e.message}. Trying to reconnect in 5 seconds...`)
+    this.tryReconnect()
+  }
+
+  onTunnelClose () {
+    this.log('Tunnels closed unexpectedly. Trying to reconnect in 5 seconds...')
+    this.tryReconnect()
+  }
+
+  runServer (selfInit) {
+    this.options = _.get(this.serverless, 'service.custom.ngrokTunnel', {})
+
+    if (this.options.envPath) {
+      this.noEnvFile = false
+      this.envPath = path.resolve(process.cwd(), this.options.envPath)
+
+      try {
+        this.envContent = envFile.parseFileSync(this.envPath)
+      } catch (e) {
+        this.envContent = {}
+        this.noEnvFile = true
+      }
+    }
+    if (this.slsOptions.tunnel === 'true' || selfInit) {
+      if (this.options.tunnels && this.options.tunnels.length) {
+        this.options.tunnels.forEach((opt) => this.runTunnel(opt))
+        process.on('SIGINT', () => this.stopTunnel())
+      } else {
+        this.log('Tunnels are not configured. Skipping...')
+      }
+    }
+  }
+
+  stopTunnel () {
+    this.log('Disconnecting from tunnels...')
+    ngrok.kill()
+    if (!this.noEnvFile) {
+      (this.options.tunnels || []).forEach(({envProp}) => {
+        delete this.envContent[envProp]
+      })
+      this.writeToEnv()
+    }
+  }
+
+  tryReconnect () {
+    if (!this.reconnectTried) {
+      setTimeout(() => {
+        (this.options.tunnels || []).forEach((opt) => this.runTunnel(opt))
+      }, 5000)
+      this.reconnectTried = true
+    }
+  }
+
+  writeToEnv () {
+    if (!this.noEnvFile) {
+      fs.writeFileSync(this.envPath, envFile.stringifySync(this.envContent))
+    }
+  }
+}
+
+module.exports = ServerlessTunnel
